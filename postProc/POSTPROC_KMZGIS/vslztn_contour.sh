6e9cf6d707779521799deda53b2388e8cb0f65bc
FL-ASGS <ptaeb@coconut.dmes.fit.edu>
2019-02-15 18:56:30 -0500

    vslztn_contour.sh update, convert on background

--- postProc/POSTPROC_KMZGIS/vslztn_contour.sh
+++ postProc/POSTPROC_KMZGIS/vslztn_contour.sh
@@ -3,31 +3,24 @@
 RUNDIR=$1
 OUTPUTDIR=$2
 cycleDIR=$3
-HOSTNAME=$4
-ENSTORM=$5
-CONFIG=$6
-FIGUREGENEXECUTABLE=$7   
-NORTH=$8   
-SOUTH=$9
-EAST=${10}
-WEST=${11}
-SYSLOG=${12}
-stage=${13}
+ENSTORM=$4
+CONFIG=$5
+FIGUREGENEXECUTABLE=$6
+SYSLOG=$7
+stage=$8
 #
-# GENERAL SET UP
-#
-. $CONFIG # grab all static config info
+. $CONFIG     
 . ${mainDIR}/src/logging.sh
 cd $RUNDIR             
-#
+
 # set path to the POSTPROC_KMZGIS directory
 POSTPROC_DIR=$OUTPUTDIR/POSTPROC_KMZGIS
+
 # OUTPUTPREFIX : set output filename prefix
 OUTPUTPREFIX=${cycleDIR}.vis
-#
+
 # ######################################################################################
 #                        .. JPG ..
-#   perl make_JPG.pl --storm 01 --year 2006 --adv 05 --n 30.5 --s 28.5 --e -88.5 --w -90.5 --outputprefix 01_2006_nhcconsensus_05
 #
 JPGLOGFILE=$RUNDIR/jpg.log # log file for jpg-related info/errors
 date >> $JPGLOGFILE
@@ -42,60 +35,57 @@ fi
 #
 mkdir $RUNDIR/Temp 2>> $JPGLOGFILE
 if [[ $stage -eq 1 ]]; then
-   FIGUREGENTEMPLATE=$OUTPUTDIR/POSTPROC_KMZGIS/FigGen/Elev_gif_s1.inp
-else 
-   FIGUREGENTEMPLATE=$OUTPUTDIR/POSTPROC_KMZGIS/FigGen/Elev_gif_s2.inp
+   FIGUREGENTEMPLATE_elev='full-ele-wind.inp'  
+   FIGUREGENTEMPLATE_wave='full-hs-dir.inp'
+else
+   FIGUREGENTEMPLATE_elev='irl-ele-wind.inp'
+   FIGUREGENTEMPLATE_wave='irl-hs-dir.inp' 
 fi
-cp $POSTPROC_DIR/FigGen/Elev_asgs.pal $RUNDIR 2>> $JPGLOGFILE
-cp $POSTPROC_DIR/FigGen/FIT.eps       $RUNDIR 2>> $JPGLOGFILE
-cp $POSTPROC_DIR/FigGen/Labels.txt    $RUNDIR 2>> $JPGLOGFILE
-cp $POSTPROC_DIR/FigGen/coast.dot.EFL.txt $RUNDIR 2>> $JPGLOGFILE
+
+# Copying control files, Labels, and other utilites
+if [[ $stage -eq 1 ]]; then
+   cp $OUTPUTDIR/POSTPROC_KMZGIS/FigGen/GIFs/Elev_MS.pal                 $RUNDIR 2>> $JPGLOGFILE
+   cp $OUTPUTDIR/POSTPROC_KMZGIS/FigGen/GIFs/HS.pal                      $RUNDIR 2>> $JPGLOGFILE
+   cp $OUTPUTDIR/POSTPROC_KMZGIS/FigGen/GIFs/fulldomain_background.txt   $RUNDIR 2>> $JPGLOGFILE
+   cp $OUTPUTDIR/POSTPROC_KMZGIS/FigGen/GIFs/fulldomain_background.pgw   $RUNDIR 2>> $JPGLOGFILE
+   cp $OUTPUTDIR/POSTPROC_KMZGIS/FigGen/GIFs/fulldomain_background.png   $RUNDIR 2>> $JPGLOGFILE
+   cp $OUTPUTDIR/POSTPROC_KMZGIS/FigGen/GIFs/full-ele-wind.inp           $RUNDIR 2>> $JPGLOGFILE
+   cp $OUTPUTDIR/POSTPROC_KMZGIS/FigGen/GIFs/full-hs-dir.inp             $RUNDIR 2>> $JPGLOGFILE
+else
+   cp $OUTPUTDIR/POSTPROC_KMZGIS/FigGen/GIFs/irl-ele-wind.inp            $RUNDIR 2>> $JPGLOGFILE
+   cp $OUTPUTDIR/POSTPROC_KMZGIS/FigGen/GIFs/irl-hs-dir.inp              $RUNDIR 2>> $JPGLOGFILE
+   cp $OUTPUTDIR/POSTPROC_KMZGIS/FigGen/GIFs/Elev_MS.pal                 $RUNDIR 2>> $JPGLOGFILE
+   cp $OUTPUTDIR/POSTPROC_KMZGIS/FigGen/GIFs/HS.pal                      $RUNDIR 2>> $JPGLOGFILE
+   cp $OUTPUTDIR/POSTPROC_KMZGIS/FigGen/GIFs/irl-background.txt          $RUNDIR 2>> $JPGLOGFILE
+   cp $OUTPUTDIR/POSTPROC_KMZGIS/FigGen/GIFs/irl-background.pgw          $RUNDIR 2>> $JPGLOGFILE
+   cp $OUTPUTDIR/POSTPROC_KMZGIS/FigGen/GIFs/irl-background.png          $RUNDIR 2>> $JPGLOGFILE
+   cp $OUTPUTDIR/POSTPROC_KMZGIS/FigGen/GIFs/irl-labels.txt              $RUNDIR 2>> $JPGLOGFILE
+fi
+
+# Redirect to run directory
 cd $RUNDIR
-perl ${POSTPROC_DIR}/FigGen/make_JPG.pl --figuregen_template $FIGUREGENTEMPLATE --adv $cycleDIR --n $NORTH --s $SOUTH --e $EAST --w $WEST --outputprefix $OUTPUTPREFIX  2>> $JPGLOGFILE 2>&1
+
+# Logging
 logMessage "Generating 2D GIF contour for predicted surface elevation"
-mpirun -np $NCPU $POSTPROC_DIR/FigGen/${FIGUREGENEXECUTABLE} -I FigGen_${OUTPUTPREFIX}.inp >> $JPGLOGFILE 2>&1 
-rm -rf Temp 2>> $JPGLOGFILE
+
+# Running the FigureGen for elevation and wind
+mpirun -np $NCPU $POSTPROC_DIR/FigGen/${FIGUREGENEXECUTABLE} -I ${FIGUREGENTEMPLATE_elev} >> $JPGLOGFILE 2>&1 
+
+# Running the FigureGen for elevation and wind
+mpirun -np $NCPU $POSTPROC_DIR/FigGen/${FIGUREGENEXECUTABLE} -I ${FIGUREGENTEMPLATE_wave} >> $JPGLOGFILE 2>&1
+
 date >> $JPGLOGFILE
-convert -delay 18 *.jpg -loop 0 elev_wind.gif
+if [[ $stage -eq 1 ]]; then
+   convert -delay 18 FullElevWin*.jpg -loop 0 full_elev_wind.gif  &
+   convert -delay 18 FullHsDir*.jpg   -loop 0 full_hs_dir.gif     &
+else
+   convert -delay 18 IRLElevWin*.jpg -loop 0 irl_elev_wind.gif    &
+   convert -delay 18 IRLHsDir*.jpg   -loop 0 irl_hs_dir.gif       &
+fi
+
+# Cleaning
+rm -rf Temp 2>> $JPGLOGFILE
 rm *.jpg
-rm *.inp # removing previous input files
+# rm *.inp 
 #
 #
-#			Static  elevation
-#	
-# Use the default FigureGen template if it has not been specified
-if [[ $stage -eq 1 ]]; then
-   FIGUREGENTEMPLATE=$OUTPUTDIR/POSTPROC_KMZGIS/FigGen/MaxEle_s1.inp  
-else
-   FIGUREGENTEMPLATE=$OUTPUTDIR/POSTPROC_KMZGIS/FigGen/MaxEle_s2.inp   
-fi
-mkdir $RUNDIR/Temp 2>> $JPGLOGFILE
-cp $POSTPROC_DIR/FigGen/Elev_asgs.pal $RUNDIR 2>> $JPGLOGFILE
-cp $POSTPROC_DIR/FigGen/FIT.eps       $RUNDIR 2>> $JPGLOGFILE
-cd $RUNDIR/
-perl ${POSTPROC_DIR}/FigGen/make_JPG.pl --figuregen_template $FIGUREGENTEMPLATE --adv $cycleDIR --n $NORTH --s $SOUTH --e $EAST --w $WEST --outputprefix $OUTPUTPREFIX  2>> $JPGLOGFILE 2>&1
-logMessage "Generating 2D contour for maximum predicted surface elevation"
-mpirun -np $NCPU $POSTPROC_DIR/FigGen/${FIGUREGENEXECUTABLE} -I FigGen_${OUTPUTPREFIX}.inp >> $JPGLOGFILE 2>&1 
-rm -rf Temp 2>> $JPGLOGFILE
-date >> $JPGLOGFILE
-rm *.inp # removing previous input files
-#
-#
-#                       Static  Hs
-#
-# Use the default FigureGen template if it has not been specified
-if [[ $stage -eq 1 ]]; then
-   FIGUREGENTEMPLATE=$OUTPUTDIR/POSTPROC_KMZGIS/FigGen/MaxHS_s1.inp
-else
-   FIGUREGENTEMPLATE=$OUTPUTDIR/POSTPROC_KMZGIS/FigGen/MaxHS_s2.inp
-fi
-mkdir $RUNDIR/Temp 2>> $JPGLOGFILE
-cp $POSTPROC_DIR/FigGen/wavht.pal     $RUNDIR 2>> $JPGLOGFILE
-cp $POSTPROC_DIR/FigGen/FIT.eps       $RUNDIR 2>> $JPGLOGFILE
-cd $RUNDIR/
-perl ${POSTPROC_DIR}/FigGen/make_JPG.pl --figuregen_template $FIGUREGENTEMPLATE --adv $cycleDIR --n $NORTH --s $SOUTH --e $EAST --w $WEST --outputprefix $OUTPUTPREFIX  2>> $JPGLOGFILE 2>&1
-logMessage "Generating 2D contour for maximum predicted Hs"
-mpirun -np $NCPU $POSTPROC_DIR/FigGen/${FIGUREGENEXECUTABLE} -I FigGen_${OUTPUTPREFIX}.inp >> $JPGLOGFILE 2>&1
-rm -rf Temp 2>> $JPGLOGFILE
-date >> $JPGLOGFILE
-rm *.inp # removing previous input files
